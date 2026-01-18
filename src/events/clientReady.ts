280e9ec21e662fa9e9cbe9ea9e9b5476b379327e
waffle <csshin9928@gmail.com>
2026-01-18 19:09:04 +0900

    feat: add clientReady event handler with custom activity status

---
+++ src/events/clientReady.ts
@@ -0,0 +1,15 @@
+import { ActivityType, Client } from "discord.js";
+import type { Event } from "../handlers/event-handler.js";
+
+export default {
+  name: "clientReady",
+  once: true,
+  async execute(client: Client) {
+    console.info(`Logged in as ${client.user?.tag}`);
+
+    client.user?.setActivity({
+      name: "Lunatic | /help",
+      type: ActivityType.Custom,
+    });
+  },
+} as Event;
