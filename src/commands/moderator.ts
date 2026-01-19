e1c6e39bd5a84ac5a3a46572e6f16450fa73e1cb
waffle <csshin9928@gmail.com>
2026-01-19 11:39:19 +0900

    feat: add moderator command with add/remove subcommands

---
+++ src/commands/moderator.ts
@@ -0,0 +1,46 @@
+import { updateGuild } from "@/db/guilds.js";
+import type { Command } from "@/handlers/command-handler.js";
+import { SlashCommandBuilder } from "discord.js";
+
+const command: Command = {
+  data: new SlashCommandBuilder()
+    .setName("moderator")
+    .setDescription("Manage moderators")
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("add")
+        .setDescription("Add a moderator")
+        .addUserOption((option) =>
+          option
+            .setName("user")
+            .setDescription("The user to add as a moderator")
+            .setRequired(true),
+        ),
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("remove")
+        .setDescription("Remove a moderator")
+        .addUserOption((option) =>
+          option
+            .setName("user")
+            .setDescription("The user to remove as a moderator")
+            .setRequired(true),
+        ),
+    ),
+  execute: async (_, interaction) => {
+    try {
+      await updateGuild(interaction.guildId!, {
+        moderators: [interaction.options.getUser("user")!.id],
+      });
+    } catch (error) {
+      console.error(`Error updating guild ${interaction.guildId}`);
+    }
+    await interaction.reply({
+      content: "Moderator added successfully!",
+      ephemeral: true,
+    });
+  },
+};
+
+export default command;
